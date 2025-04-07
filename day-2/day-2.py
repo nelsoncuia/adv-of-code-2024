197e6ab1ed8efa005b8982a492e9acbbd82e6b2c
nelson-rodrigues <nelsoncuia23@gmail.com>
2025-04-07 00:10:42 -0300

    day 2 advancements

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -1,29 +1,21 @@
 # Open the file and read the content
-with open('/Users/nelsonrodrigues/Projects/advent-of-code-2024/day-2/input-day-2.txt', 'r') as file:
+with open(r"C:\Users\nelso\Projects\Advent of Code 2024\adv-of-code-2024\day-2\input-day-2.txt", 'r') as file:
     lines = file.readlines()
 
 
-list1 = []
+def is_list_decreasing(lst):
+    # Check if the list is strictly decreasing
+    return all(x > y for x, y in zip(lst, lst[1:]))
+
+def is_list_increasing(lst):
+    # Check if the list is strictly increasing
+    return all(x < y for x, y in zip(lst, lst[1:]))
+
+
+input_list = []
 
 
 # Iterate through each line and split by spaces
 for line in lines:
     words = line.split()  # This will split by any whitespace
-    list1.append(words)
-
-int_list1 = []
-
-for word in list1:
-    int_list1.append([int(x) for x in word])
-
-
-for report in int_list1:
-    for i in range(1:len(report)):
-        if report[i] 
-    
-        
-    
-    
-
-
-print(int_list1)
+    input_list.append([int(word) for word in words])
